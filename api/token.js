f6326110f76a85ed276644064e55631c84a7b7a2
andybark <107442952+andybark@users.noreply.github.com>
2026-04-25 18:11:27 -0400

    Add files via upload

--- api/token.js
+++ api/token.js
@@ -1,11 +1,6 @@
 export default function handler(req, res) {
-  const clientId = process.env.TWITCH_CLIENT_ID;
-  const accessToken = process.env.TWITCH_ACCESS_TOKEN;
-  
   res.status(200).json({
-    clientIdExists: !!clientId,
-    accessTokenExists: !!accessToken,
-    clientIdLength: clientId ? clientId.length : 0,
-    accessTokenLength: accessToken ? accessToken.length : 0
+    clientId: process.env.TWITCH_CLIENT_ID,
+    accessToken: process.env.TWITCH_ACCESS_TOKEN
   });
 }
